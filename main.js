d6e756dba06aebd35e9620bc2e2d59dfc3834089
FarisPalayi <farispalayi@gmail.com>
2023-04-06 16:18:52 +0530

    add tab functionality

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-import './style.css'
+import "./style.css";
 
+const addActive = (element) => element.classList.add("active");
+const rmvActive = (element) => element.classList.remove("active");
+const hideAll = (elms) => elms.forEach(rmvActive);
+
+const tabs = document.querySelectorAll("[data-tab]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+function selectTab(tab) {
+  const tabName = tab.dataset.tab;
+  const tabContent = document.querySelector(`[data-content="${tabName}"]`);
+
+  hideAll(tabs)
+  addActive(tab);
+  hideAll(tabContents);
+  addActive(tabContent);
+}
+
+tabs.forEach((tab) => tab.addEventListener("click", () => selectTab(tab)));
